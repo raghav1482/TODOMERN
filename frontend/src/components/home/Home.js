0d41f45be359842fcf2cf52044b1c4fc0395fbca
raghav_1482 <raghvendra1482002@gmail.com>
2023-11-26 04:05:35 +0530

    UI fixes

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import "../home/home.css";
-
+import {useNavigate} from 'react-router-dom'
 export default function Home(){
+    const nav = useNavigate();
+    const gototodo=()=>{
+        nav("/todo");
+    }
     return(<>
     <div className='homediv'>
         <div className='home1'>
         <h1>Organise your tasks</h1>
         <p>Streamline tasks, set reminders, and boost productivity effortlessly with our user-friendly todo list website. Simplify and accomplish efficiently.</p>
-        <button>Make TODO List</button>
+        <button onClick={gototodo}>Make TODO List</button>
         </div>
         <div className='home2'>
         <p>Welcome to our innovative and user-friendly todo list website! Designed to streamline and organize your tasks, our platform offers a seamless way to manage your daily activities and goals. With intuitive features, customizable options, and easy accessibility, our todo list website is your ultimate productivity companion, empowering you to prioritize, track, and accomplish your tasks efficiently.</p>
